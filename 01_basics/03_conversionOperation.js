b1867a24c892fa1e01410f27303f04aabf7b840a
Mohit Kumar <130058153+mohitsingh4716@users.noreply.github.com>
2023-11-26 14:58:31 +0000

    conversionOperation added

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber= Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+
+// "33"=> 33
+//"33abc"=> NaN ->(not a number)
+//true => 1; false => 0
+
+let isLoggedIn= ""
+
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1=> true ; o=> false
+//"" => false (emty string)
+// "mohit" => true
+
+let someNumber= 33;
+let sringNumber= String(someNumber)
+
+console.log(sringNumber);
+console.log(typeof sringNumber);
