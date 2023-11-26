55d4375818042f9aa49e9d837933c0bb0eaf323e
Mohit Kumar <130058153+mohitsingh4716@users.noreply.github.com>
2023-11-26 14:24:44 +0000

    datatypes with examples

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,35 @@
+"use strict"; // treat all js code as newer version
+
+
+// alert(3+3) // we are not using nodejs  not browser
+
+console.log(3+3) 
+/// code readiability  should be high
+
+console.log("Mohit")
+
+let name= "mohit"
+let age = 19
+let isLoggedIn=false
+
+  //* primitive data type
+// number => 2 to power 53
+// bigint
+//string=> " "
+//boolean=> true/false
+//null=> standalone value (empty)
+// undefine=> not assign value
+// symbol => unique
+
+
+// object 
+
+console.log(typeof "mohit");
+
+console.log(typeof age);
+
+console.log(typeof null); //object 
+
+console.log(typeof undefine); //undefine
+
+
