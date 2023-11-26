cc02389e996bd932f6538eec4a8a7aa7cbf22e21
Mohit Kumar <130058153+mohitsingh4716@users.noreply.github.com>
2023-11-26 15:50:06 +0000

    string to number conversion and assign

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "33"
 
-console.log(typeof score);
-console.log(typeof (score));
+//console.log(typeof score);
+//console.log(typeof (score));
 
 let valueInNumber= Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber)
 
 
 // "33"=> 33
@@ -15,7 +15,7 @@ console.log(valueInNumber)
 let isLoggedIn= ""
 
 let booleanIsLoggedIn= Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
 
 // 1=> true ; o=> false
 //"" => false (emty string)
@@ -24,5 +24,44 @@ console.log(booleanIsLoggedIn)
 let someNumber= 33;
 let sringNumber= String(someNumber)
 
-console.log(sringNumber);
-console.log(typeof sringNumber);
+//console.log(sringNumber);
+//console.log(typeof sringNumber);
+
+
+//******************************  Operation  ******************************  
+
+let value =3;
+let negValue= -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(3**2);
+// console.log(2/3);
+// console.log(2%2); //imp used in crypto
+
+let str1 ="Mohit"
+let str2= " Kumar"
+
+let str3= str1 +str2
+console.log(str3);
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2");// 12
+// console.log("1" + 2 +2); //=> 122
+// console.log(1+ 2 + "2"); // =>32
+
+//console.log(+true); // =>convert to 1
+
+//console.log(+""); // "" means empty convert to 0
+
+let gamecounter=100
+const a= ++gamecounter;
+console.log(a); //101
+ const b= gamecounter++
+console.log(b); //101
+console.log(gamecounter);  // 102
+
+
+
