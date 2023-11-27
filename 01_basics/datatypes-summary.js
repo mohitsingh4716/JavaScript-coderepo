9c3688a8767f3f8b74832735c56d5bb72119a772
Mohit Kumar <130058153+mohitsingh4716@users.noreply.github.com>
2023-11-27 08:29:49 +0000

    datatypes-summary added

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,33 @@
+//#Primitive
+//7 types { string, Number Booleans , null, undefined, symbol BigInt}
+
+const score= 100
+const scorevalue= 100.3
+
+const isLoggedIn= false
+const outsideTemp = null
+let userEmail ; //{undefined}
+
+//console.log(userEmail);
+
+const id= Symbol('123')
+const newId= Symbol('123')
+
+//console.log(id==newId);
+//console.log(newId);
+
+const BigInt= 4366746343846233;
+console.log(BigInt)
+
+//#Referece (non- preemitive)
+ // Array, Object , functions
+const heros=["mohit", "Bhanu", "Rishabh", "somiya"]
+// let myobject {
+//     name: "Mohit"
+//     age: 22;
+// }
+
+const myfunction= function(){
+    console.log("Hello MOhit")
+}
+myfunction()
